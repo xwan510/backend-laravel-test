97b79ebb0dea46fbe323819c27a0cb8e365cc559
Edison Wang <xwan510@gmail.com>
2020-05-19 12:51:27 +1000

    Switch CI to use Postgres
    Mysql doesn't have good support of cast to float. It has to deal with long syntax and decimal only.
    Postgres is much more friendly. Just stick with pgsql.

--- app/Http/Controllers/API/V1/PropertyAnalyticsReportController.php
+++ app/Http/Controllers/API/V1/PropertyAnalyticsReportController.php
@@ -40,29 +40,29 @@ class PropertyAnalyticsReportController extends Controller
                         ->join('analytic_types', 'analytic_types.id', '=', 'property_analytics.analytic_type_id')
                         ->where('properties.'.$validatedData['filter'], '=', $validatedData['value'])
                         ->select(DB::raw('
-                                        analytic_types.id,
-                                        analytic_types.name,
-                                        analytic_types.units,
-                                        analytic_types.is_numeric,
-                                        analytic_types.num_decimal_places,
-                                        min(cast(value as float)) as min,
-                                        max(cast(value as float)) as max,
-                                        percentile_cont(0.5) within group (order by cast(value as float)) as median,
-                                        concat(
-                                            cast(round(
-                                                count(value)::decimal * 100 / '.$total.',
-                                                2
-                                            ) as float),
-                                            \'%\'
-                                        ) as has_value,
-                                        concat(
-                                            cast(round(
-                                                ('.$total.' - count(value)::decimal) * 100 / '.$total.',
-                                                2
-                                            ) as float),
-                                            \'%\'
-                                        ) as has_no_value
-                                        '))
+                            analytic_types.id,
+                            analytic_types.name,
+                            analytic_types.units,
+                            analytic_types.is_numeric,
+                            analytic_types.num_decimal_places,
+                            min(value::float) as min,
+                            max(value::float) as max,
+                            percentile_cont(0.5) within group (order by value::float) as median,
+                            concat(
+                                round(
+                                    count(value)::decimal * 100 / '.$total.',
+                                    2
+                                )::float,
+                                \'%\'
+                            ) as has_value,
+                            concat(
+                                round(
+                                    ('.$total.' - count(value)::decimal) * 100 / '.$total.',
+                                    2
+                                )::float,
+                                \'%\'
+                            ) as has_no_value
+                        '))
                         ->groupBy('analytic_types.id')
                         ->get();
         return response()->json($response, 200);
